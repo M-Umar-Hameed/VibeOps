7bdb0d64875129da63c458dc4db90afec40f21af
M-Umar-Hameed <umerh1209@gmail.com>
2026-07-11 21:24:23 +0500

    removed dead test files

--- test-actors.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "./src/db/client.js";
-import { actors } from "./src/db/schema.js";
-
-async function run() {
-  const allActors = await db.select().from(actors);
-  console.log("Actors in DB:", allActors.length);
-  console.log(allActors);
-}
-run().catch(console.error);
--- test-tickets.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "./src/db/client.js";
-import { tickets } from "./src/db/schema.js";
-
-async function run() {
-  const rows = await db.select().from(tickets).limit(2);
-  console.log(rows);
-}
-run().catch(console.error);
